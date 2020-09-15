1e0e4ba3a80a42a51d31c71a0cb72e55fd521fe4
Madhava Di Florio <mattdiflorio@gmail.com>
2020-09-15 17:53:53 +0200

    Send connection request to a tracker

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ mod torrents;
 
 use anyhow;
 use bytebuffer::ByteBuffer;
-use std::net::UdpSocket;
+use rand::Rng;
+use std::{net::UdpSocket, time::Duration};
 use url::Url;
 
 fn main() -> anyhow::Result<()> {
@@ -31,20 +32,21 @@ fn main() -> anyhow::Result<()> {
 
     {
         let mut socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+        socket.set_read_timeout(Some(Duration::new(5, 0)))?;
 
         let connReq = buildConnReq();
 
         socket
-            .connect("tracker.leechers-paradise.org:6969")
+            .connect("tracker.opentrackr.org:1337")
             .expect("couldn't connect to address");
 
         socket
             .send(&connReq.to_bytes())
             .expect("couldn't send message");
 
-        let mut buf = [0; 200];
-        match socket.recv(&mut buf) {
-            Ok(received) => println!("received {} bytes {:?}", received, &buf[..received]),
+        let mut recv_buf = [0; 16];
+        match socket.recv(&mut recv_buf) {
+            Ok(received) => println!("received {} bytes {:?}", received, &recv_buf[..received]),
             Err(e) => println!("recv function failed: {:?}", e),
         }
     }
@@ -52,9 +54,20 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn buildConnReq() -> ByteBuffer {
+    let mut rng = rand::thread_rng();
     let mut buffer = ByteBuffer::new();
-    buffer.write_u64(0x41727101980);
-    buffer.write_u32(0);
-    buffer.write_u32(98766687);
+
+    // 0       64-bit integer  protocol_id     0x41727101980 // magic constant
+    // 8       32-bit integer  action          0 // connect
+    // 12      32-bit integer  transaction_id
+
+    let protocol_id: i64 = 0x41727101980;
+    let action: i32 = 0;
+    let transaction_id: i32 = rng.gen::<i32>();
+
+    buffer.write_i64(protocol_id);
+    buffer.write_i32(action);
+    buffer.write_i32(transaction_id);
+
     return buffer;
 }
