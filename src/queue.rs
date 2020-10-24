8d72b53484767851a2894d2727fcca242876db20
Madhava Di Florio <mattdiflorio@gmail.com>
2020-10-24 18:42:06 +0200

    Added deque, peek and len to Queue struct.

--- src/queue.rs
+++ src/queue.rs
@@ -38,14 +38,17 @@ impl Queue<'_> {
         }
     }
 
+    /// Remove the first item from the pieces queue.
     pub fn deque(&mut self) {
         self.pieces.pop_front();
     }
 
+    /// Get the first item in pieces queue.
     pub fn peek(self) -> PieceBlock {
         return self.pieces[0];
     }
 
+    /// Get the length of the pieces queue.
     pub fn len(self) -> usize {
         return self.pieces.len();
     }
